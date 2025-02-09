2e5599afe0fb4d5126274ebd8867e5795cf77dd6
gdeotale <gdeotale@gmail.com>
2025-02-09 19:32:45 +0530

    Add files via upload

---
+++ config/dino_swin_monkey2.py
@@ -0,0 +1,357 @@
+dataset_type = 'CocoDataset'
+data_root = '/mnt/prj002/Monkey_Challenge/Monkey_Challenge_Dataset/g_data/40x/Data_40x_512/'
+backend_args = None
+train_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RandomFlip', prob=0.5),
+    dict(
+        type='RandomChoice',
+        transforms=[[{
+            'type':
+            'RandomChoiceResize',
+            'scales': [(480, 1333), (512, 1333), (544, 1333), (576, 1333),
+                       (608, 1333), (640, 1333), (672, 1333), (704, 1333),
+                       (736, 1333), (768, 1333), (800, 1333)],
+            'keep_ratio':
+            True
+        }],
+                    [{
+                        'type': 'RandomChoiceResize',
+                        'scales': [(400, 4200), (500, 4200), (600, 4200)],
+                        'keep_ratio': True
+                    }, {
+                        'type': 'RandomCrop',
+                        'crop_type': 'absolute_range',
+                        'crop_size': (384, 600),
+                        'allow_negative_crop': True
+                    }, {
+                        'type':
+                        'RandomChoiceResize',
+                        'scales': [(480, 1333), (512, 1333), (544, 1333),
+                                   (576, 1333), (608, 1333), (640, 1333),
+                                   (672, 1333), (704, 1333), (736, 1333),
+                                   (768, 1333), (800, 1333)],
+                        'keep_ratio':
+                        True
+                    }]]),
+    dict(type='PackDetInputs')
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PackDetInputs',
+        meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                   'scale_factor'))
+]
+train_dataloader = dict(
+    batch_size=3,
+    num_workers=8,
+    persistent_workers=True,
+    sampler=dict(type='DefaultSampler', shuffle=True),
+    batch_sampler=dict(type='AspectRatioBatchSampler'),
+    dataset=dict(
+        type='CocoDataset',
+        data_root=
+        '/mnt/prj002/Monkey_Challenge/Monkey_Challenge_Dataset/g_data/40x/Data_40x_512/',
+        ann_file='json2/train.json',
+        data_prefix=dict(img='train/'),
+        filter_cfg=dict(filter_empty_gt=False, min_size=32),
+        pipeline=[
+            dict(type='LoadImageFromFile', backend_args=None),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(type='RandomFlip', prob=0.5),
+            dict(
+                type='RandomChoice',
+                transforms=[[{
+                    'type':
+                    'RandomChoiceResize',
+                    'scales': [(480, 1333), (512, 1333), (544, 1333),
+                               (576, 1333), (608, 1333), (640, 1333),
+                               (672, 1333), (704, 1333), (736, 1333),
+                               (768, 1333), (800, 1333)],
+                    'keep_ratio':
+                    True
+                }],
+                            [{
+                                'type': 'RandomChoiceResize',
+                                'scales': [(400, 4200), (500, 4200),
+                                           (600, 4200)],
+                                'keep_ratio': True
+                            }, {
+                                'type': 'RandomCrop',
+                                'crop_type': 'absolute_range',
+                                'crop_size': (384, 600),
+                                'allow_negative_crop': True
+                            }, {
+                                'type':
+                                'RandomChoiceResize',
+                                'scales':
+                                [(480, 1333), (512, 1333), (544, 1333),
+                                 (576, 1333), (608, 1333), (640, 1333),
+                                 (672, 1333), (704, 1333), (736, 1333),
+                                 (768, 1333), (800, 1333)],
+                                'keep_ratio':
+                                True
+                            }]]),
+            dict(type='PackDetInputs')
+        ],
+        backend_args=None,
+        metainfo=dict(
+            classes=('lymphocyte', 'monocyte'),
+            palette=[(220, 20, 60), (60, 220, 60)])))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type='CocoDataset',
+        data_root=
+        '/mnt/prj002/Monkey_Challenge/Monkey_Challenge_Dataset/g_data/40x/Data_40x_512/',
+        ann_file='json2/val.json',
+        data_prefix=dict(img='val/'),
+        test_mode=True,
+        pipeline=[
+            dict(type='LoadImageFromFile', backend_args=None),
+            dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(
+                type='PackDetInputs',
+                meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                           'scale_factor'))
+        ],
+        backend_args=None,
+        metainfo=dict(
+            classes=('lymphocyte', 'monocyte'),
+            palette=[(220, 20, 60), (60, 220, 60)])))
+test_dataloader = dict(
+    batch_size=1,
+    num_workers=2,
+    persistent_workers=True,
+    drop_last=False,
+    sampler=dict(type='DefaultSampler', shuffle=False),
+    dataset=dict(
+        type='CocoDataset',
+        data_root=
+        '/mnt/prj002/Monkey_Challenge/Monkey_Challenge_Dataset/g_data/40x/Data_40x_512/',
+        ann_file='json2/val.json',
+        data_prefix=dict(img='val/'),
+        test_mode=True,
+        pipeline=[
+            dict(type='LoadImageFromFile', backend_args=None),
+            dict(type='Resize', scale=(1333, 800), keep_ratio=True),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(
+                type='PackDetInputs',
+                meta_keys=('img_id', 'img_path', 'ori_shape', 'img_shape',
+                           'scale_factor'))
+        ],
+        backend_args=None,
+        metainfo=dict(
+            classes=('lymphocyte', 'monocyte'),
+            palette=[(220, 20, 60), (60, 220, 60)])))
+val_evaluator = dict(
+    type='CocoMetric',
+    ann_file=
+    '/mnt/prj002/Monkey_Challenge/Monkey_Challenge_Dataset/g_data/40x/Data_40x_512/json2/val.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=None,
+    metric_items=None)
+test_evaluator = dict(
+    type='CocoMetric',
+    ann_file=
+    '/mnt/prj002/Monkey_Challenge/Monkey_Challenge_Dataset/g_data/40x/Data_40x_512/json2/val.json',
+    metric='bbox',
+    format_only=False,
+    backend_args=None,
+    metric_items=None)
+default_scope = 'mmdet'
+default_hooks = dict(
+    timer=dict(type='IterTimerHook'),
+    logger=dict(type='LoggerHook', interval=100),
+    param_scheduler=dict(type='ParamSchedulerHook'),
+    checkpoint=dict(
+        type='CheckpointHook', interval=5, max_keep_ckpts=2, save_best='auto'),
+    sampler_seed=dict(type='DistSamplerSeedHook'),
+    visualization=dict(type='DetVisualizationHook'))
+env_cfg = dict(
+    cudnn_benchmark=False,
+    mp_cfg=dict(mp_start_method='fork', opencv_num_threads=0),
+    dist_cfg=dict(backend='nccl'))
+vis_backends = [dict(type='LocalVisBackend')]
+visualizer = dict(
+    type='DetLocalVisualizer',
+    vis_backends=[
+        dict(type='LocalVisBackend'),
+        dict(type='TensorboardVisBackend')
+    ],
+    name='visualizer')
+log_processor = dict(type='LogProcessor', window_size=50, by_epoch=True)
+log_level = 'INFO'
+load_from = './checkpoints/dino-5scale_swin-l_8xb2-36e_coco-5486e051.pth'
+resume = False
+model = dict(
+    type='DINO',
+    num_queries=900,
+    with_box_refine=True,
+    as_two_stage=True,
+    data_preprocessor=dict(
+        type='DetDataPreprocessor',
+        mean=[123.675, 116.28, 103.53],
+        std=[58.395, 57.12, 57.375],
+        bgr_to_rgb=True,
+        pad_size_divisor=1),
+    backbone=dict(
+        type='SwinTransformer',
+        pretrain_img_size=384,
+        embed_dims=192,
+        depths=[2, 2, 18, 2],
+        num_heads=[6, 12, 24, 48],
+        window_size=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        qk_scale=None,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        drop_path_rate=0.2,
+        patch_norm=True,
+        out_indices=(0, 1, 2, 3),
+        with_cp=True,
+        convert_weights=True,
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint=
+            'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth'
+        )),
+    neck=dict(
+        type='ChannelMapper',
+        in_channels=[192, 384, 768, 1536],
+        kernel_size=1,
+        out_channels=256,
+        act_cfg=None,
+        norm_cfg=dict(type='GN', num_groups=32),
+        num_outs=5),
+    encoder=dict(
+        num_layers=6,
+        layer_cfg=dict(
+            self_attn_cfg=dict(embed_dims=256, num_levels=5, dropout=0.0),
+            ffn_cfg=dict(
+                embed_dims=256, feedforward_channels=2048, ffn_drop=0.0))),
+    decoder=dict(
+        num_layers=6,
+        return_intermediate=True,
+        layer_cfg=dict(
+            self_attn_cfg=dict(embed_dims=256, num_heads=8, dropout=0.0),
+            cross_attn_cfg=dict(embed_dims=256, num_levels=5, dropout=0.0),
+            ffn_cfg=dict(
+                embed_dims=256, feedforward_channels=2048, ffn_drop=0.0)),
+        post_norm_cfg=None),
+    positional_encoding=dict(
+        num_feats=128, normalize=True, offset=0.0, temperature=20),
+    bbox_head=dict(
+        type='DINOHead',
+        num_classes=2,
+        sync_cls_avg_factor=True,
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=5.0),
+        loss_iou=dict(type='GIoULoss', loss_weight=2.0)),
+    dn_cfg=dict(
+        label_noise_scale=0.5,
+        box_noise_scale=1.0,
+        group_cfg=dict(dynamic=True, num_groups=None, num_dn_queries=100)),
+    train_cfg=dict(
+        assigner=dict(
+            type='HungarianAssigner',
+            match_costs=[
+                dict(type='FocalLossCost', weight=2.0),
+                dict(type='BBoxL1Cost', weight=5.0, box_format='xywh'),
+                dict(type='IoUCost', iou_mode='giou', weight=2.0)
+            ])),
+    test_cfg=dict(max_per_img=300),
+    num_feature_levels=5)
+optim_wrapper = dict(
+    type='OptimWrapper',
+    optimizer=dict(type='AdamW', lr=8e-05, weight_decay=0.05),
+    paramwise_cfg=dict(
+        norm_decay_mult=0, bias_decay_mult=0, bypass_duplicate=True))
+max_epochs = 60
+train_cfg = dict(type='EpochBasedTrainLoop', max_epochs=60, val_interval=1)
+val_cfg = dict(type='ValLoop')
+test_cfg = dict(type='TestLoop')
+param_scheduler = [
+    dict(type='LinearLR', start_factor=1e-05, by_epoch=False, begin=0, end=10),
+    dict(
+        type='CosineAnnealingLR',
+        eta_min=8.000000000000001e-07,
+        begin=15,
+        end=60,
+        T_max=35,
+        by_epoch=True,
+        convert_to_iter_based=True)
+]
+auto_scale_lr = dict(base_batch_size=16)
+pretrained = 'https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22k.pth'
+num_levels = 5
+train_batch_size_per_gpu = 3
+train_num_workers = 8
+stage2_num_epochs = 1
+base_lr = 8e-05
+metainfo = dict(
+    classes=('lymphocyte', 'monocyte'), palette=[(220, 20, 60), (60, 220, 60)])
+train_pipeline_stage2 = [
+    dict(type='LoadImageFromFile', backend_args=None),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='RandomResize',
+        scale=(640, 640),
+        ratio_range=(0.1, 2.0),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=(640, 640)),
+    dict(
+        type='PhotoMetricDistortion',
+        brightness_delta=32,
+        contrast_range=(0.5, 1.5),
+        saturation_range=(0.5, 1.5),
+        hue_delta=18),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='Pad', size=(640, 640), pad_val=dict(img=(114, 114, 114))),
+    dict(type='PackDetInputs')
+]
+custom_hooks = [
+    dict(
+        type='PipelineSwitchHook',
+        switch_epoch=59,
+        switch_pipeline=[
+            dict(type='LoadImageFromFile', backend_args=None),
+            dict(type='LoadAnnotations', with_bbox=True),
+            dict(
+                type='RandomResize',
+                scale=(640, 640),
+                ratio_range=(0.1, 2.0),
+                keep_ratio=True),
+            dict(type='RandomCrop', crop_size=(640, 640)),
+            dict(
+                type='PhotoMetricDistortion',
+                brightness_delta=32,
+                contrast_range=(0.5, 1.5),
+                saturation_range=(0.5, 1.5),
+                hue_delta=18),
+            dict(type='RandomFlip', prob=0.5),
+            dict(
+                type='Pad', size=(640, 640),
+                pad_val=dict(img=(114, 114, 114))),
+            dict(type='PackDetInputs')
+        ])
+]
+launcher = 'none'
+work_dir = './work_dirs/dino_swin_monkey2'
